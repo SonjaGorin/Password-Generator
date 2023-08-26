4e6a22a8d1deb31d8f02c349afe3435b1b095f24
Sonja <sonjaljatkovic@gmail.com>
2023-08-26 10:57:24 -0400

    added prompt variables

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+  var uppercaseConfirm = confirm("Do you want to include uppercase letters in your password?");
+  alert(uppercaseConfirm);
+  var lowercaseConfirm = confirm("Do you want to include lowercase letters in your password?");
+  alert(lowercaseConfirm);
+  var numbersConfirm = confirm("Do you want to include numbers in your password?");
+  alert(numbersConfirm);
+  var specialCharactersConfirm = confirm("Do you want to include special characters in your password?");
+  alert(specialCharactersConfirm);
+  var passwordLengthPrompt = prompt("Choose the length of your password. Type th enumber between 8 and 128.");
+  alert(passwordLengthPrompt);
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
