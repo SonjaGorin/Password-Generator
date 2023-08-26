ce3dd9ca8491694391f202b0b2c5be1469b24c7a
Sonja <sonjaljatkovic@gmail.com>
2023-08-26 11:12:34 -0400

    added arrays of characters

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Assignment Code
+uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+specialCharacters = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[']
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -15,7 +20,7 @@ function writePassword() {
   var passwordLengthPrompt = prompt("Choose the length of your password. Type th enumber between 8 and 128.");
   alert(passwordLengthPrompt);
 
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
